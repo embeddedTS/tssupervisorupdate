e47c9f71da1d6298f7f0e15cdb4d10c85a38a0b0
Mark Featherston <mark@embeddedts.com>
2024-08-27 09:38:28 -0700

    Clean up clutter in single .c file.
    Split up into the current supported update architectures, now calling
    v0 / v1 instead of ts7970/common.
    
    Use compatible strings for detecting the different boards.

---
+++ update-v0.h
@@ -0,0 +1,6 @@
+#pragma once
+
+int do_v0_micro_update(board_t *board, int i2cfd, char *update_path);
+int do_v0_micro_get_rev(board_t *board, int i2cfd, int *revision);
+int do_v0_micro_get_file_rev(board_t *board, int *revision, char *update_path);
+int do_v0_micro_print_info(board_t *board, int i2cfd);
